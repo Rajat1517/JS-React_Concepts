8add489f7334ce9dd522c4b5fabda8b5d750e38b
Rajat Mishra <rajatmishra0815@gmail.com>
2025-08-30 09:41:59 +0530

    Added debounce and throttle in closure concept

--- concepts/closure.js
+++ concepts/closure.js
@@ -37,3 +37,48 @@ console.log("decrement by 5:", decrement(5));
 console.log("increment by 19:", increment(19));
 
 //  One other very common application of closure is debouncing of function. It is added under the performance section.
+
+const debounce = (fn, delay) => {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
+};
+
+const printX = debounce((x) => console.log(x), 200);
+
+for (let i = 0; i < 5; i++) {
+  printX(i);
+}
+
+// this will only print 4, as the printX function is debounced and on every iteration when it is called, the timeout of the debouncing funciton resets. Hence we get the output of the last call.
+// This is different than the let and var code snippet output, as this resembles with the twisted use of var to print the viable output. In that another fucntion is called inside the loop giving var a newer scope, here alse the printX funcion works the same.
+
+// Another common application of closure in UI performace is "Throttling". It lets the funitnality to be executed in a fixed interval even if called multiple times within the interval.
+//  lets UI to not break if the user spam the functionality creting a load on the browser.
+
+const throttle = (fn, interval) => {
+  let last;
+  return (...args) => {
+    const now = Date.now();
+    if (!last || now - last >= interval) {
+      last = now;
+      fn(...args);
+    }
+  };
+};
+
+
+
+const printY= throttle((y)=>console.log(y),200);
+
+for(let i=100;i<1000;i+=100){
+  setTimeout(()=>{
+    printY(i);
+  },i)
+}
+
+
