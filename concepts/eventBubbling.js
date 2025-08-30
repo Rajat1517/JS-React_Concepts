45b64532e882683c2916e5ff573bb16e65d8152d
Rajat Mishra <rajatmishra0815@gmail.com>
2025-08-30 10:20:45 +0530

    Added definiton of event bubbling

---
+++ concepts/eventBubbling.js
@@ -0,0 +1,9 @@
+// Another imp concept related to evetns in React
+
+// In react if an event is triggered on a child element, it popogates to the ancestors until document is reached.
+//  This is called event bubbling.
+
+
+// this is can be stopped at any level by using: e.stopPropagation(); or e.stopImmediatePropagation();
+
+// Can find the usage of this for Dragover and Drop event in Form Builder project.
