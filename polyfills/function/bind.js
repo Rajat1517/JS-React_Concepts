4f794397cccbc8ce682458d3963bd8cb0e012789
Rajat Mishra <rajatmishra0815@gmail.com>
2025-11-29 13:46:26 +0530

    Updated the crucial nuance in the bind polyfill, Missed earlier

--- polyfills/function/bind.js
+++ polyfills/function/bind.js
@@ -26,13 +26,27 @@ console.log(rafale_fly("target2", "target3"));
 // Implementing polyfill
 
 Function.prototype.myBind = function (...args) {
-  const cb = this,
-    arguments = args.splice(1);
-
+  const cb = this;
+  const params = args.splice(1);
+  // we do this cb trick, because in case of regular functions as nested function declaration the implicit this binding is lost and default binding applies. To captutre and explicitly hold the implicit from outer fn, we keep it in cb
+  // we can work without it as well, covered in myBind2
   return function (...extraArgs) {
-    return cb.call(args[0], ...[...arguments, ...extraArgs]);
+    return cb.call(args[0], ...[...params, ...extraArgs]);
   };
 };
+
+
+Function.prototype.myBind2= function (thisContext, ...args){
+
+  // As we return an arrow function, now the implicit this binding is kept entact. Because the arrow fuction depends on lexical binding and the closest this binding in its lexical scope is the outer regular function is implicitly bound to the caller fn. Hence that is passed in the nested fn as this.
+
+  return (...extraArgs)=>{
+    return this.call(thisContext,...args, ...extraArgs );
+  }
+}
+
+
+// 
 
 const rafale_fly2 = su_30_mki.fly.myBind(rafale);
 
